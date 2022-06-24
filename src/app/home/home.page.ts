ae4a4fd069ccde1fb9ce2c0f07f5d0ad47f7a988
parxdoxe <dumoulin.florian22@gmail.com>
2022-06-24 10:55:23 +0200

    search

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,6 +13,8 @@ export class HomePage implements OnInit {
   films: any = []
   filmDetail: any = []
   search: string = ''
+  click: boolean = false
+  searchB: any = []
 
   constructor(private http: HttpClient, private storage: Storage) {}
 
@@ -24,8 +26,14 @@ export class HomePage implements OnInit {
   }
 
   ngOnInit() {
-    this.storage.create()
-    this.storage.get('search').then(search => this.search = search)
+    
+  }
+
+  searchThis(event){
+    console.log(this.search);
+    this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=40009de0f135cfd09989d99f18892b45&query=${this.search}`).subscribe( response => {
+      this.searchB = response['results']
+      console.log(this.searchB);})
   }
   
 
